bfcf21c761a149cc777e75fd1ea5984e383296d3
Escalante Hugo <palofito40@gmail.com>
2026-07-02 01:07:30 -0300

    chore: inicialización del proyecto, package.json y app.js base

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get ('/', (req, res) =>{
+res.send('Servidor Funcionando')
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor corriendo en http://localhost:${PORT}`)
+})
