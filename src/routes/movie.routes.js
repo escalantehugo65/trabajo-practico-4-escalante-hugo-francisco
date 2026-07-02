250f1cda74a9d7c6fc9321ddf330073ba4d0fba7
Escalante Hugo <palofito40@gmail.com>
2026-07-02 02:31:07 -0300

    feat: vincular rutas con los controladores del CRUD

--- src/routes/movie.routes.js
+++ src/routes/movie.routes.js
@@ -1,25 +1,22 @@
 import { Router } from 'express';
+import { 
+    getAllMovies, 
+    getMovieById, 
+    createMovie, 
+    updateMovie, 
+    deleteMovie 
+} from '../controllers/movie.controllers.js';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('Prueba: Listar todas las películas');
-});
+router.get('/', getAllMovies);
 
-router.get(`/:id`, (req, res) => {
-    res.send(`Prueba: Obtener película con ID ${req.params.id}`);
-});
+router.get('/:id', getMovieById);
 
-router.post('/', (req, res) => {
-    res.send('Prueba: Crear una película');
-});
+router.post('/', createMovie);
 
-router.put(`/:id`, (req, res) => {
-    res.send(`Prueba: Actualizar película con ID ${req.params.id}`);
-});
+router.put('/:id', updateMovie);
 
-router.delete(`/:id`, (req, res) => {
-    res.send(`Prueba: Eliminar película con ID ${req.params.id}`);
-});
+router.delete('/:id', deleteMovie);
 
 export default router;
